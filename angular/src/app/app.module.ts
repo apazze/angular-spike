eabdead3951d09e1fcd5c1c07747454ba07d0edc
Alisson Pazze <alissonp.zeta@gmail.com>
2023-08-15 16:53:11 -0300

    Criado primeiro modulo e segundo componente

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -4,15 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { MeuPrimeiroComponenteComponent } from './meu-primeiro-componente/meu-primeiro-componente.component';
 
-@NgModule({
+@NgModule({ //Decorator ou anotação da classe que é um módulo e que agrupa e organiza componentes, services, etc... de uma determinado pacote ou feature
   declarations: [ // declarations, imports, providers, bootstrap são metadados
-    AppComponent,
+    AppComponent, // em declarations é declarado os componentes, diretivas e pipes do módulo
     MeuPrimeiroComponenteComponent //expor o componente para outras classes
   ],
   imports: [
-    BrowserModule
+    BrowserModule // módulos externos que serão importados nos componentes ou neste módulo
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [], // serviços para o módulo. Neste caso, como se trata do AppModule ele serão globais, exemplo de uso: serviço de Autenticação ou guardas de rotad devem ser declarados aqui, ficará de escopo para toda a aplicação
+  bootstrap: [AppComponent] //somente está disponivel neste módulo raíz, este AppComponent é o componente principal, onde ficará o menu, rotas, etc...
 })
 export class AppModule { }
---
+++ angular/src/app/meu-primeiro-modulo/meu-primeiro-modulo.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SegundoComponenteComponent } from './segundo-componente/segundo-componente.component';
+
+
+
+@NgModule({
+  declarations: [
+    SegundoComponenteComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class MeuPrimeiroModuloModule { }
