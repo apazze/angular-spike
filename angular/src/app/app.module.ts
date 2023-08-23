d6811e0a49f725dfdb4e5b1127bb3643767582f9
Alisson Pazze <alissonp.zeta@gmail.com>
2023-08-23 08:47:30 -0300

    diretiva ngStyle

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -14,13 +14,14 @@ import { DiretivaNgIfComponent } from './diretiva-ng-if/diretiva-ng-if.component
 import { DiretivaNgswitchComponent } from './diretiva-ngswitch/diretiva-ngswitch.component';
 import { DiretivaNgforComponent } from './diretiva-ngfor/diretiva-ngfor.component';
 import { DiretivaNgclassComponent } from './diretiva-ngclass/diretiva-ngclass.component';
+import { DiretivaNgstyleComponent } from './diretiva-ngstyle/diretiva-ngstyle.component';
 
 @NgModule({ //Decorator ou anotação da classe que é um módulo e que agrupa e organiza componentes, services, etc... de uma determinado pacote ou feature
   declarations: [ // declarations, imports, providers, bootstrap são metadados
     AppComponent, // em declarations é declarado os componentes, diretivas e pipes do módulo
     MeuPrimeiroComponenteComponent, 
     DataBindingComponent, 
-    InputPropertyComponent, OutputPropertyComponent, CicloComponent, DiretivaNgIfComponent, DiretivaNgswitchComponent, DiretivaNgforComponent, DiretivaNgclassComponent //expor o componente para outras classes
+    InputPropertyComponent, OutputPropertyComponent, CicloComponent, DiretivaNgIfComponent, DiretivaNgswitchComponent, DiretivaNgforComponent, DiretivaNgclassComponent, DiretivaNgstyleComponent //expor o componente para outras classes
   ],
   imports: [
     BrowserModule, // módulos externos que serão importados nos componentes ou neste módulo
