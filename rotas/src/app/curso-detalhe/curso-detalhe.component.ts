407afa5ef98a81fc3b53931b2547b5f5caa02e16
apazze <apazze@users.noreply.github.com>
2023-09-10 16:01:31 -0300

    Maneira correta de pegar o id da rota

--- rotas/src/app/curso-detalhe/curso-detalhe.component.ts
+++ rotas/src/app/curso-detalhe/curso-detalhe.component.ts
@@ -1,18 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-curso-detalhe',
   templateUrl: './curso-detalhe.component.html',
   styleUrls: ['./curso-detalhe.component.css']
 })
-export class CursoDetalheComponent {
+export class CursoDetalheComponent implements OnInit, OnDestroy {
 
   id!: string;
 
+  inscricao!: Subscription;
+
   constructor(private route: ActivatedRoute) {
-    this.id = this.route.snapshot.params['id']; // para pegar a chave id do params que é um Object dinamico
-    console.log(this.route);
+    // this.id = this.route.snapshot.params['id']; // para pegar a chave id do params que é um Object dinamico
+    // console.log(this.route);
+  }
+
+  ngOnInit() {
+    this.inscricao = this.route.params.subscribe(parametro => this.id = parametro['id']);
+  }
+
+  ngOnDestroy() {
+    this.inscricao.unsubscribe(); //boa pratica, se desinscrever
   }
 
 }
