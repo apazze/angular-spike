b20aacd85432d8b4a605e3e6ab47769d986becb6
apazze <apazze@users.noreply.github.com>
2023-09-10 19:11:03 -0300

    Isolando modulo de rotas para cursos. Ideal que cada modulo da aplicacao (Cursos) contenha seus sub componentes (cursoNaoEncontrado, cursoDetalhe), sua service, sua Module e seu ModuleRoutes

--- rotas/src/app/app.routing.ts
+++ rotas/src/app/app.routing.ts
@@ -1,18 +1,18 @@
-import { ModuleWithProviders } from '@angular/core';
-import { RouterModule, Routes } from "@angular/router";
+// import { ModuleWithProviders } from '@angular/core';
+// import { RouterModule, Routes } from "@angular/router";
 
-import { HomeComponent } from "./home/home.component";
-import { LoginComponent } from "./login/login.component";
-import { CursosComponent } from "./cursos/cursos.component";
-import { CursoDetalheComponent } from "./cursos/curso-detalhe/curso-detalhe.component";
-import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
+// import { HomeComponent } from "./home/home.component";
+// import { LoginComponent } from "./login/login.component";
+// import { CursosComponent } from "./cursos/cursos.component";
+// import { CursoDetalheComponent } from "./cursos/curso-detalhe/curso-detalhe.component";
+// import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
 
-const APP_ROUTES: Routes = [
-    { path: '', component: HomeComponent },
-    { path: 'login', component: LoginComponent }, //quando o link faz parte do root a barra "/" é opcional
-    { path: 'cursos', component: CursosComponent },
-    { path: 'curso/:id', component: CursoDetalheComponent },
-    { path: 'naoEncontrado', component: CursoNaoEncontradoComponent },
-];
+// const APP_ROUTES: Routes = [
+//     { path: '', component: HomeComponent },
+//     { path: 'login', component: LoginComponent }, //quando o link faz parte do root a barra "/" é opcional
+//     { path: 'cursos', component: CursosComponent },
+//     { path: 'curso/:id', component: CursoDetalheComponent },
+//     { path: 'naoEncontrado', component: CursoNaoEncontradoComponent },
+// ];
 
-export const routing: ModuleWithProviders<any> = RouterModule.forRoot(APP_ROUTES);
+// export const routing: ModuleWithProviders<any> = RouterModule.forRoot(APP_ROUTES);
--- rotas/src/app/cursos/cursos.module.ts
+++ rotas/src/app/cursos/cursos.module.ts
@@ -4,13 +4,13 @@ import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
 import { CursosService } from './cursos.service';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
 import { MatListModule } from '@angular/material/list';
+import { CursosRoutingModule } from './cursos.routing.module';
 
 @NgModule({
     imports: [
         CommonModule, // diretivas ngIf, ngFor
-        RouterModule,
+        CursosRoutingModule,
         MatListModule
     ],
     exports: [],
