b33a729c43b34ab86d76ff04889610878a5cd340
apazze <apazze@users.noreply.github.com>
2023-09-17 16:49:02 -0300

    canLoad mais uma proteção contra carregamento de um modulo onde o usuario pode nao ter o privilegio. Foi testado apos logar na aplicação, na aba rede do debbuger no navegador após atualizar a pagina e ser redirecionado novamente para login o modulo foi baixado novamente mas nao deveria, o canLoad resolve isso

--- rotas/src/app/app.routing.module.ts
+++ rotas/src/app/app.routing.module.ts
@@ -9,10 +9,22 @@ import { CursosGuard } from "./guard/cursos.guard";
 // import { AlunosGuard } from "./guard/alunos.guard.";
 
 const appRoutes: Routes = [
-    { path: 'cursos', loadChildren: () => import('./cursos/cursos.module').then(m => m.CursosModule), canActivate: [AuthGuard], canActivateChild: [CursosGuard]}, // Guarda de Rotas, da acesso aos componentes somente se tiver autenticado (canActivate)
-    { path: 'alunos', loadChildren: () => import('./alunos/alunos.module').then(m => m.AlunosModule), canActivate: [AuthGuard]/* , canActivateChild: [AlunosGuard] */}, // O child pode-se aplicar por exemplo pra ver se o usuario tem acesso para editar. Movido para o escopo local de Alunos
-    { path: '', component: HomeComponent, canActivate: [AuthGuard]},
-    { path: 'login', component: LoginComponent}, //quando o link faz parte do root a barra "/" é opcional
+    {
+        path: 'cursos',
+        loadChildren: () => import('./cursos/cursos.module').then(m => m.CursosModule),
+        canActivate: [AuthGuard],  // Guarda de Rotas, da acesso aos componentes somente se tiver autenticado (canActivate)
+        canActivateChild: [CursosGuard],
+        canLoad: [AuthGuard] // canLoad outra proteção contra carregamento de um modulo onde o usuario posso nao ter permissao de acesso
+    },
+    {
+        path: 'alunos',
+        loadChildren: () => import('./alunos/alunos.module').then(m => m.AlunosModule),
+        canActivate: [AuthGuard],
+        /* , canActivateChild: [AlunosGuard] O child pode-se aplicar por exemplo pra ver se o usuario tem acesso para editar. Movido para o escopo local de Alunos */
+        canLoad: [AuthGuard]
+    },
+    { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+    { path: 'login', component: LoginComponent }, //quando o link faz parte do root a barra "/" é opcional
 ];
 
 @NgModule({
--- rotas/src/app/guard/auth.guard.ts
+++ rotas/src/app/guard/auth.guard.ts
@@ -1,29 +1,41 @@
 import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, CanLoad, Route, Router, RouterStateSnapshot, UrlSegment, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthService } from '../login/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard implements CanActivate, CanLoad {
 
   constructor(
     private authService: AuthService,
     private router: Router
   ) { }
-  
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot
-  ) : Observable<boolean> | boolean {
+  ): Observable<boolean> | boolean {
     console.log('AuthGuard')
-    if(this.authService.usuarioEstaAutenticado()) {
+    
+    return this.verificarAcesso();
+  }
+
+  private verificarAcesso() {
+    if (this.authService.usuarioEstaAutenticado()) {
       return true;
     }
 
     this.router.navigate(['/login']);
-    
+
     return false;
+  }
+
+  canLoad(
+    route: Route
+  ): Observable<boolean> | Promise<boolean> | boolean {
+    console.log('canLoad: verificando se usuario pode carregar o codigo do modulo')
+    return this.verificarAcesso();
   }
 }
