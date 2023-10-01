13f06f0474de6c59b58bfde82bf23dd78fc2dace
apazze <apazze@users.noreply.github.com>
2023-10-01 20:51:15 -0300

    Chamada http para viacep

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-template-form',
@@ -11,6 +12,7 @@ export class TemplateFormComponent {
   //   nome: 'Alisson',
   //   email: 'alisson@email.com'
   // }
+  constructor(private http: HttpClient) {}
 
   usuario: any = {
     nome: null,
@@ -21,5 +23,21 @@ export class TemplateFormComponent {
   onSubmit(form: any) {
     console.log(form)
     console.log(this.usuario)
+  }
+
+  consultaCep(cep: any) {
+    var filterCep = (cep.target as HTMLInputElement).value;
+    filterCep = filterCep.replace(/\D/g, ''); // só digitos
+
+    if (filterCep != "") {
+      //Expressão regular para validar o CEP.
+      var validacep = /^[0-9]{8}$/;
+      if(validacep.test(filterCep)) {
+        this.http.get(`https://viacep.com.br/ws/${filterCep}/json`)
+        .subscribe(r => console.log(r));
+      }
+    }
+
+    console.log(filterCep)
   }
 }
