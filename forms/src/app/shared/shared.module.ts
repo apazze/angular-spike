70704c71b9ae951639057c3ef7f0a5a6686dfdaf
apazze <apazze@users.noreply.github.com>
2023-12-03 16:43:13 -0300

    Para prover um serviço, configurar em providers

--- forms/src/app/shared/shared.module.ts
+++ forms/src/app/shared/shared.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormDebugComponent } from './form-debug/form-debug.component';
 import { CampoControlErroComponent } from './campo-control-erro/campo-control-erro.component';
+import { DropDownService } from './services/drop-down.service';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,9 @@ import { CampoControlErroComponent } from './campo-control-erro/campo-control-er
   exports: [
     FormDebugComponent,
     CampoControlErroComponent
+  ],
+  providers: [
+    DropDownService
   ]
 })
 export class SharedModule { }
